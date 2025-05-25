d5e29f4b7f3e3f20552116d5446cd1c70bff60fa
James Stevens <jimmystevo@hotmail.com>
2025-05-26 10:34:28 +1200

    ATIMS-14, set up structure and vectors for inventory system and started the user choice section

---
+++ InventoryManagementSystem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+//setting structure to house vectors for editing stock numbers / prices and adding new / removing old items
+struct Inventory {
+	vector<int> iStock;
+	vector<string> sItemName;
+	vector<double> dPrice;
+};
+
+int main{
+	while (i != 1) {
+		cout << "Select an option: ";
+		cout << ""
+	}
+}
