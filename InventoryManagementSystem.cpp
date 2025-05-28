dd6fc9b800a22da0dbda44d8be282be23dac2ccd
James Stevens <jimmystevo@hotmail.com>
2025-05-29 10:35:51 +1200

    ATIMS-14 updated functions and added comments

--- InventoryManagementSystem.cpp
+++ InventoryManagementSystem.cpp
@@ -11,40 +11,136 @@ struct MyInv {
 	double dPrice;
 };
 
+// function to update the amount of stock from an Admin side
+int UpdateStock(int iStockUpdate) {
+	cout << "Please enter the new stock amount:\n";
+	cin >> iStockUpdate;
+	cin.ignore();
+
+	return iStockUpdate;
+	// add error handling here!!!! should only detect integers!!!!!
+};
+
+// function for updating the price of the stored items in the vector
+double UpdatePrice(double dPriceUpdate) {
+	cout << "Please enter the new price;\n";
+	cin >> dPriceUpdate;
+	cin.ignore();
+
+	return dPriceUpdate;
+	// add error handling here!!!! should only detect doubles!!!!!
+
+};
+
+string ChangeName(string sChangeName) {
+	sChangeName = "Ting";
+	return sChangeName;
+};
+
+void AddNew();
+
+void Remove();
+
+void ConvertString(string& s) {
+	for (int i = 0; i < s.length(); ++i) {
+		s[i] = toupper(s[i]);
+	}
+}
+
 int main() {
+
+
 	//setting vectors up for use with the structure
 	vector<MyInv> iInv{
-		{"Mahuka Honey", 20, 1000.00},
-		{"Jade Stones", 20, 200.50},
-		{"Kiwi Plushie", 50, 25.00},
-		{"All Blacks Jersey", 6, 60.00},
-		{"Black Caps Jersey", 34, 55.00},
-		{"Stickers", 100, 5.75},
-		{"Magnets", 75, 6.95},
-		{"Books", 39, 12.50},
-		{"Stationary Bundle", 42, 35.25},
-		{"ALcohol", 17, 80.64}
+		{"MAHUKA HONEY", 20, 1000.00},
+		{"JADE STONES", 20, 200.50},
+		{"KIWI PLUSHIE", 50, 25.00},
+		{"ALL BLACKS JERSEY", 6, 60.00},
+		{"BLACK CAP JERSEY", 34, 55.00},
+		{"STICKERS", 100, 5.75},
+		{"MAGNETS", 75, 6.95},
+		{"BOOKS", 39, 12.50},
+		{"STATIONARY", 42, 35.25},
+		{"ALCOHOL", 17, 80.64}
 	};
 	int iInvChoice = 0;
-	int iCount = 0;
+	string sItemSelect;
 	int i = 0;
-	int sItemSelect = 0;
 
-	while (i != 1) {
-		cout << "Select an option: ";
-		cout << "\n1. Change Stock of an Item.\n2. Change the price of an Item.\n3. Change Item Name.\n4. Add a new item.\n5. Remove an Item.\n6. Return to previous screen.\n7. Exit";
+
+	cout << "Select an option: ";
+
+	// Set a do while loop to keep the main selection present until breaking out of it for user choices in the switch cases themselves
+	do {
+
+		cout << "\n1. Change Stock of an Item.\n2. Change the price of an Item.\n3. Change Item Name.\n4. Add a new item.\n5. Remove an Item.\n6. Return to previous screen.\n7. Exit\n";
 		cin >> iInvChoice;
+
+		cin.ignore();
 
 		switch (iInvChoice) {
 		case 1:
-			for (int i = 0; i > 1; i++) {
-				cout << "\nWhich item would you like to edit?\n";
-
-
-				cin >> sItemSelect;
+			//for loop to list out the current stock, accounts for potentiall increased / decreased vector
+			for (int i = 0; i < iInv.size(); ++i) {
+				cout << iInv[i].sItemName << " \t " << iInv[i].iStock << "\t" << iInv[i].dPrice << "\n";
 			}
+			cout << "\nWhich item would you like to edit?\n" << endl;
+			getline(cin, sItemSelect);
+
+
+
+			ConvertString(sItemSelect);
+			cout << sItemSelect << endl;
+
+			i = 0;
+
+			while (iInv[i].sItemName != sItemSelect) {
+				i++;
+				// add error handling here!!!!
+				//@@
+			};
+			if (iInv[i].sItemName == sItemSelect) {
+				cout << "succuss" << endl;
+			}
+			else {
+				cout << "Enter a valid selection!\n";
+			}
+
+			iInv[i].iStock = UpdateStock(iInv[i].iStock);
+
+			cout << iInv[i].iStock;
+
 			break;
 		case 2:
+			//for loop to list out the current stock, accounts for potentiall increased / decreased vector
+			for (int i = 0; i < iInv.size(); ++i) {
+				cout << iInv[i].sItemName << " \t " << iInv[i].iStock << "\t" << iInv[i].dPrice << "\n";
+			}
+			cout << "\nWhich item would you like to edit?\n" << endl;
+			getline(cin, sItemSelect);
+
+
+
+			ConvertString(sItemSelect);
+			cout << sItemSelect << endl;
+
+			i = 0;
+
+			while (iInv[i].sItemName != sItemSelect) {
+				i++;
+				// add error handling here!!!!
+				//@@
+			};
+			if (iInv[i].sItemName == sItemSelect) {
+				cout << "succuss";
+			}
+			else {
+				cout << "Enter a valid selection!";
+			}
+
+			iInv[i].dPrice = UpdatePrice(iInv[i].dPrice);
+
+			cout << iInv[i].dPrice;
 			break;
 		case 3:
 			break;
@@ -60,5 +156,6 @@ int main() {
 		default:
 			cout << "\nPlease select an option from 1 - 7.";
 		}
-	}
+	} while (iInvChoice != 7);
 };
+
