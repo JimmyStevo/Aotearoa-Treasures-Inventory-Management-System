aa3e6e58ea932b9374be88789e6a7bf8cae149f1
James Stevens <jimmystevo@hotmail.com>
2025-06-10 10:17:37 +1200

    ATIMS-33 refactor code of inventory system

--- InventoryManagementSystem.cpp
+++ InventoryManagementSystem.cpp
@@ -1,3 +1,7 @@
+// Inventory System Refactor.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+//
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -5,235 +9,183 @@
 
 using namespace std;
 
-//setting structure to house vectors for editing stock numbers / prices and adding new / removing old items
-
-
-struct MyInv {
-	string sItemName;
-	int iStock;
-	double dPrice;
-};
-
-vector<MyInv> iInv{
-{"MAHUKA HONEY", 20, 1000.00},
-{"JADE STONES", 20, 200.50},
-{"KIWI PLUSHIE", 50, 25.00},
-{"ALL BLACKS JERSEY", 6, 60.00},
-{"BLACK CAP JERSEY", 34, 55.00},
-{"STICKERS", 100, 5.75},
-{"MAGNETS", 75, 6.95},
-{"BOOKS", 39, 12.50},
-{"STATIONARY", 42, 35.25},
-{"ALCOHOL", 17, 80.64}
-};
-
-vector<MyInv>::iterator it; // set up iterator to later be able to loop through vectors and delete / add items
-
-
-class StockCheck {
-private:
+//Setup of class for the inventory base to be inherited by other classes
+class Inventory {
 public:
+    string name;
+    int stock;
+    double price;
+
+    //function to prompt the user for inventory specific variables and store them
+    void input() {
+
+        cout << "Enter the item name:\n";
+        getline(cin, name);
+        cout << "Enter the price:\n";
+        cin >> price;
+        cout << "Enter the amount of stock:\n";
+        cin >> stock;
+        cin.ignore();
+    }
+};
+
+// class set up for vector usage to hold item information and functions related to the 
+class item {
+private:
+    // function to convert strings to upper case for comparison purposes
+    void ConvertString(string& s) {
+        for (int i = 0; i < s.length(); ++i) {
+            s[i] = toupper(s[i]);
+        }
+    }
+public:
+    //sets vector for the use of items in a location
+    vector<Inventory> vInv{
+        {"MAHUKA HONEY", 20, 1000.00},
+        {"JADE STONES", 20, 200.50},
+        {"KIWI PLUSHIE", 50, 25.00},
+        {"ALL BLACKS JERSEY", 6, 60.00},
+        {"BLACK CAP JERSEY", 34, 55.00},
+        {"STICKERS", 100, 5.75},
+        {"MAGNETS", 75, 6.95},
+        {"BOOKS", 39, 12.50},
+        {"STATIONARY", 42, 35.25},
+        {"ALCOHOL", 17, 80.64}
+
+    };
+
+    vector<Inventory>::iterator it;
+
+    //function to add new stock to the vector using the base class of Inventory to prompt the user questions
+    void addStock() {
+        Inventory Stock;
+        Stock.input();
+        vInv.push_back(Stock);
+    }
+
+    //iterator to display the table of items for the location specified
+    void display() {
+        cout << "Products listed at wellington:\n";
+        cout << setw(20) << "Name" << setw(15) << "Stock" << setw(20) << "Price" << "\n";
+        for (auto& Inventory : vInv) {
+            cout << setw(20) << Inventory.name << setw(15) << Inventory.stock << setw(20) << fixed << setprecision(2) << Inventory.price << "\n";
+        }
+    }
+
+    // function to grab the name of the item to update the stock, then update stock with a legitimate number / integer
+    void stockcheck(int iStock, int iNumber) {
+
+        if (iStock < 5) {
+            cout << vInv[iNumber].name << " needs to be restocked";
+        }
+        else {
+            cout << "Current stock of " << vInv[iNumber].name << " is " << vInv[iNumber].stock << endl;
+        }
+    }
 
 
+
+    //function for selecting an item, allows for multiple selection to then direct to the correct function after
+    string SelectItem() {
+        string sUpdate;
+        display();
+
+        cout << "Which item would you like to update?";
+        getline(cin, sUpdate);
+        return sUpdate;
+
+    }
+
+    void ChangeStockName(string s) {
+        string sNewName, tempname;
+        ConvertString(s);
+
+        for (auto& Inventory : vInv) {
+            tempname = Inventory.name;
+            ConvertString(tempname);
+
+            if (tempname == s) {
+                cout << "What would you like the new name to be?\n";
+                getline(cin, sNewName);
+                Inventory.name = sNewName;
+            }
+        }
+    }
+    // functionb using an iterator to find the parsed item name and delete it from the records
+    void RemoveItem(string s) {
+        ConvertString(s);
+        for (auto it = vInv.begin(); it != vInv.end(); ) {
+            if (it->name == s) {
+                it = vInv.erase(it);
+                cout << "removed successfully";
+            }
+            else {
+                ++it;
+            }
+        }
+
+    }
+
+    void UpdateItemPrice(string s) {
+        ConvertString(s);
+
+    }
+
+    void UpdateItemStock(string s) {
+        ConvertString(s);
+    }
+};
+
+class UserInvSelection {
+public:
+    int iChoice;
+    item checks;
+    string sChoice;
+
+    void SelectingOptions() {
+        do {
+            cout << "\n1. Add New Item.\n2. Change Item Name.\n3. Change the price of an Item.\n4. Change Stock of an Item.\n5. Remove an Item.\n6. Return to previous screen.\n7. Exit\n";
+            cin >> iChoice;
+
+            switch (iChoice) {
+            case 1:
+                cin.ignore();
+                checks.addStock();
+                break;
+            case 2:
+                cin.ignore();
+                checks.ChangeStockName(checks.SelectItem());
+                break;
+            case 3:
+                cin.ignore();
+                checks.UpdateItemPrice(checks.SelectItem());
+                break;
+            case 4:
+                cin.ignore();
+                checks.UpdateItemStock(checks.SelectItem());
+                break;
+            case 5:
+                cin.ignore();
+                checks.RemoveItem(checks.SelectItem());
+                break;
+            case 6:
+                cin.ignore();
+                break;
+            case 7:
+                cin.ignore();
+                break;
+            default:
+                cout << "Please choose a valid selection between 1 and 7!";
+            }
+        } while (iChoice != 7);
+    }
 
 };
 
-void Check(int iStockCheck) {
-	if (iInv[iStockCheck].iStock <= 5) {
-		cout << iInv[iStockCheck].sItemName << " needs to be restocked current stock is: " << iInv[iStockCheck].iStock;
-	}
+
+int main()
+{
+    UserInvSelection Choose;
+
+    Choose.SelectingOptions();
 }
-
-void ConvertString(string& s) {
-	for (int i = 0; i < s.length(); ++i) {
-		s[i] = toupper(s[i]);
-	}
-}
-
-void DisplayProducts() {
-	cout << "Products listed at wellington:\n";
-	cout << setw(20) << "Name" << setw(15) << "Stock" << setw(20) << "Price" << "\n";
-	for (auto& MyInv : iInv) {
-		cout << setw(20) << MyInv.sItemName << setw(15) << MyInv.iStock << setw(20) << MyInv.dPrice << "\n";
-	}
-
-}
-// function to update the amount of stock from an Admin side
-int UpdateStock(int iStockUpdate) {
-	cout << "Please enter the new stock amount:\n";
-	cin >> iStockUpdate;
-	cin.ignore();
-
-	return iStockUpdate;
-	// add error handling here!!!! should only detect integers!!!!!
-};
-
-// function for updating the price of the stored items in the vector
-double UpdatePrice(double dPriceUpdate) {
-	cout << "Please enter the new price;\n";
-	cin >> dPriceUpdate;
-	cin.ignore();
-
-	return dPriceUpdate;
-	// add error handling here!!!! should only detect doubles!!!!!
-
-};
-
-string ChangeName(string sChangeName) {
-	sChangeName = "Ting";
-	return sChangeName;
-};
-
-
-int AddNew() {
-	//call function to list out the stock with their prices
-	string sItemSelect;
-	DisplayProducts();
-
-	cout << "\nWhich item would you like to edit?\n" << endl;
-	getline(cin, sItemSelect);
-
-
-
-	ConvertString(sItemSelect);
-	cout << sItemSelect << endl;
-
-	int i = 0;
-
-	while (iInv[i].sItemName != sItemSelect) {
-		i++;
-		// add error handling here!!!!
-		//@@
-	};
-	if (iInv[i].sItemName == sItemSelect) {
-		cout << "success" << endl;
-	}
-	else {
-		cout << "Enter a valid selection!\n";
-	}
-
-	iInv[i].iStock = UpdateStock(iInv[i].iStock);
-
-
-	if (iInv[i].iStock <= 5) {
-		cout << iInv[i].sItemName << " needs to be restocked current stock is: " << iInv[i].iStock;
-	}
-	return i;
-};
-
-void Remove();
-
-
-
-
-
-int main() {
-
-	//setting vectors up for use with the structure
-
-
-
-	int iInvChoice = 0;
-	string sItemSelect;
-	int i = 0;
-	int iAddInv;
-	double dAddPrice;
-
-
-
-	cout << "Select an option: ";
-
-	// Set a do while loop to keep the main selection present until breaking out of it for user choices in the switch cases themselves
-	do {
-
-		cout << "\n1. Change Stock of an Item.\n2. Change the price of an Item.\n3. Change Item Name.\n4. Add a new item.\n5. Remove an Item.\n6. Return to previous screen.\n7. Exit\n";
-		cin >> iInvChoice;
-
-		cin.ignore();
-
-		switch (iInvChoice) {
-		case 1:
-			AddNew();
-			//Check(iInv[i].iStock);
-
-			cout << iInv[i].iStock;
-
-			break;
-		case 2:
-			//call function to list out the stock with their prices
-
-			DisplayProducts();
-			cout << "\nWhich item would you like to edit?\n" << endl;
-			getline(cin, sItemSelect);
-
-
-
-			ConvertString(sItemSelect);
-			cout << sItemSelect << endl;
-
-			i = 0;
-
-			while (iInv[i].sItemName != sItemSelect) {
-				i++;
-				// add error handling here!!!!
-				//@@
-			};
-			if (iInv[i].sItemName == sItemSelect) {
-				cout << "succuss";
-			}
-			else {
-				cout << "Enter a valid selection!";
-			}
-
-			iInv[i].dPrice = UpdatePrice(iInv[i].dPrice);
-
-			cout << iInv[i].dPrice;
-			break;
-		case 3:
-			break;
-		case 4:
-			cout << "\nEnter a Product Name\n" << endl;
-			getline(cin, sItemSelect);
-
-
-			cout << "\nEnter the Amount of stock for the product:\n" << endl;
-			cin >> iAddInv;
-
-
-			cout << "\nEnter the Price of the product:\n" << endl;
-			cin >> dAddPrice;
-
-			iInv.push_back({ sItemSelect, iAddInv, dAddPrice });
-
-			DisplayProducts();
-
-			break;
-		case 5: //case for removing an item in the vector
-			DisplayProducts();
-			cout << "what would you like to remove?:" << endl;
-			getline(cin, sItemSelect);
-
-			ConvertString(sItemSelect);
-
-			//for loop using the iterator to iterate through the vector to then remove an item
-			for (auto it = iInv.begin(); it != iInv.end(); ) {
-				if (it->sItemName == sItemSelect) {
-					it = iInv.erase(it);
-				}
-				else {
-					++it;
-				}
-			}
-
-			break;
-		case 6:
-			break;
-		case 7:
-			return 0;
-			break;
-		default:
-			cout << "\nPlease select an option from 1 - 7.";
-		}
-	} while (iInvChoice != 7);
-};
-
